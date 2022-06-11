277bf90a7524fa669eaeca5ba193589cf0ff302e
Cori Lint <cori.lint@oviahealth.com>
2022-06-11 16:43:09 -0500

    Add JS and clock element to run countdown

---
+++ static/countdown.js
@@ -0,0 +1,33 @@
+const deadline = '12/10/22 6:00:00 GMT-6'
+
+function getTimeRemaining(endtime){
+    const total = Date.parse(endtime) - Date.parse(new Date());
+    const seconds = Math.floor( (total/1000) % 60 );
+    const minutes = Math.floor( (total/1000/60) % 60 );
+    const hours = Math.floor( (total/(1000*60*60)) % 24 );
+    const days = Math.floor( total/(1000*60*60*24) );
+  
+    return {
+      total,
+      days,
+      hours,
+      minutes,
+      seconds
+    };
+  }
+
+  function initializeClock(id, endtime) {
+    const clock = document.getElementById(id);
+    const timeinterval = setInterval(() => {
+      const t = getTimeRemaining(endtime);
+      clock.innerHTML = 'days: ' + t.days + '<br>' +
+                        'hours: '+ t.hours + '<br>' +
+                        'minutes: ' + t.minutes + '<br>' +
+                        'seconds: ' + t.seconds;
+      if (t.total <= 0) {
+        clearInterval(timeinterval);
+      }
+    },1000);
+  }
+
+  initializeClock('clockdiv', deadline);
