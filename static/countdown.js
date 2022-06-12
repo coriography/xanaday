e6e253b1d4e53d957fa019a9c0355b30ec8198ef
Cori Lint <cori.lint@oviahealth.com>
2022-06-11 19:34:21 -0500

    Update text and button style; fix clock delay and create spans

--- static/countdown.js
+++ static/countdown.js
@@ -17,9 +17,17 @@ function getTimeRemaining(endtime){
   }
 
   function initializeClock(id, endtime) {
-    const clock = document.getElementById(id);
-    const timeinterval = setInterval(() => {
+    const clock = document.getElementById('clockdiv');
+    const daysSpan = clock.querySelector('.days');
+    const hoursSpan = clock.querySelector('.hours');
+    const minutesSpan = clock.querySelector('.minutes');
+    const secondsSpan = clock.querySelector('.seconds');
+    function updateClock(){
       const t = getTimeRemaining(endtime);
+      daysSpan.innerHTML = ('0' + t.days).slice(-2);
+      hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+      minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+      secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
       clock.innerHTML = 'days: ' + t.days + '<br>' +
                         'hours: '+ t.hours + '<br>' +
                         'minutes: ' + t.minutes + '<br>' +
@@ -27,7 +35,10 @@ function getTimeRemaining(endtime){
       if (t.total <= 0) {
         clearInterval(timeinterval);
       }
-    },1000);
+    }
+    
+    updateClock(); // run function once at first to avoid delay
+    var timeinterval = setInterval(updateClock,1000);
   }
 
   initializeClock('clockdiv', deadline);
