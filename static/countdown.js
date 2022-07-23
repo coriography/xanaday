2eb06898038479bd63c5f30d5e6fc841f29b0602
Cori Lint <cori.lint@oviahealth.com>
2022-07-23 16:34:47 -0500

    Add responsive styling

--- static/countdown.js
+++ static/countdown.js
@@ -18,20 +18,16 @@ function getTimeRemaining(endtime){
 
   function initializeClock(id, endtime) {
     const clock = document.getElementById('clockdiv');
-    const daysSpan = clock.querySelector('.days');
-    const hoursSpan = clock.querySelector('.hours');
-    const minutesSpan = clock.querySelector('.minutes');
-    const secondsSpan = clock.querySelector('.seconds');
+    const daysSpan = clock.querySelector('#days');
+    const hoursSpan = clock.querySelector('#hours');
+    const minutesSpan = clock.querySelector('#minutes');
+    const secondsSpan = clock.querySelector('#seconds');
     function updateClock(){
       const t = getTimeRemaining(endtime);
       daysSpan.innerHTML = ('0' + t.days).slice(-2);
       hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
       minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
       secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-      clock.innerHTML = 'days: ' + t.days + '<br>' +
-                        'hours: '+ t.hours + '<br>' +
-                        'minutes: ' + t.minutes + '<br>' +
-                        'seconds: ' + t.seconds;
       if (t.total <= 0) {
         clearInterval(timeinterval);
       }
